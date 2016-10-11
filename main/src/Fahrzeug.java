d835956d280e4b8c8d9ab3a905725fbe9ec412b3
thoosequa <ralph.dworzanski@gmx.at>
2016-10-11 20:27:54 +0200

    Klassen erstellt und Layout gelegt

---
+++ main/src/Fahrzeug.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Ralph Dworzanski on 11.10.16.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ */
+
+public abstract class Fahrzeug {
+    private String marke;
+    private String modell;
+    private int baujahr;
+    private double grundpreis;
+    private int id;
+}
